001b762276c0907d7572412d5f71b869fae78c56
Slavo <slavomir.sedlak@ringier.ch>
2025-07-06 16:40:58 +0200

    build

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from "./components/canvas/EnhancedImage";
 export * from "./hooks/stickers/useStickers";
 export * from "./hooks/canvas/useImageExporter";
-export * from "./hooks/stickers/useStickers.ts";
 export type { Props } from "./components/canvas/Canvas";
